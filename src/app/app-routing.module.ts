8ac326aad8ce7cd56240d0a30d233ebccc0d56bd
Valeria <yokoyamamoto92@gmail.com>
2022-12-05 18:28:16 -0500

    path home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { Component2Component } from './components/component2/component2.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', component:Component2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
